f4a83c22086bc7c321802b9d1c77c9bf943d450c
ykc <ykc@XXX.com>
2020-04-21 15:04:07 +0900

    this is version 2 of Gugudan

--- Gugudan.java
+++ Gugudan.java
@@ -9,9 +9,17 @@ public class Gugudan {
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
 
-		System.out.println(num + "단을 출력합니다.");
-		for (int i = 1; i <= 9; i++) {
-			System.out.println(num + " * " + i + " = " + num * i);
+		if (num == 0) {
+			System.out.println("2단에서 9단까지 출력합니다.");
+			for(int j=2;j<=9;j++)
+			for (int i = 1; i <= 9; i++) {
+				System.out.println(j + " * " + i + " = " + j * i);
+			}
+		} else {
+			System.out.println(num + "단을 출력합니다.");
+			for (int i = 1; i <= 9; i++) {
+				System.out.println(num + " * " + i + " = " + num * i);
+			}
 		}
 	}
 }
